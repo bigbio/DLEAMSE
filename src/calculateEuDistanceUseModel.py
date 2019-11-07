10bb9601c6f4d2824d49a3c899ea11d41fc98038
qinchunyuan <523824050@qq.com>
2019-11-07 10:13:50 +0800

    update
    update

--- src/calculateEuDistanceUseModel.py
+++ src/calculateEuDistanceUseModel.py
@@ -32,30 +32,18 @@ class SiameseNetwork2(nn.Module):
     def __init__(self):
         super(SiameseNetwork2, self).__init__()
 
-        # self.fc1_1 = nn.Linear(34, 32)
         self.fc1_1 = nn.Linear(34, 16)
-        # self.fc1_1 = nn.Linear(61, 32)
-        self.fc1_2 = nn.Linear(16, 5)
-        # self.fc1_2 = nn.Linear(32, 5)
+        self.fc1_2 = nn.Linear(32, 5)
 
         self.cnn11 = nn.Conv1d(1, 30, 3)
         self.maxpool11 = nn.MaxPool1d(2)
-        # self.cnn12 = nn.Conv1d(30, 30, 3)
-        # self.maxpool12 = nn.MaxPool1d(6)
-        # self.cnn13 = nn.Conv1d(30, 30, 3)
-        # self.maxpool13 = nn.MaxPool1d(3)
 
         self.cnn21 = nn.Conv1d(1, 30, 3)
         self.maxpool21 = nn.MaxPool1d(2)
         self.cnn22 = nn.Conv1d(30, 30, 3)
         self.maxpool22 = nn.MaxPool1d(2)
-        # self.cnn23 = nn.Conv1d(30, 30, 3)
-        # self.maxpool23 = nn.MaxPool1d(6)
-        # self.cnn24 = nn.Conv1d(30, 30, 3)
-        # self.maxpool24 = nn.MaxPool1d(3)
 
         self.fc2 = nn.Linear(25775, 32)
-        # self.fc2 = nn.Linear(19775, 32)
 
     def forward_once(self, preInfo, fragInfo, refSpecInfo):
         preInfo = self.fc1_1(preInfo)
@@ -81,9 +69,7 @@ class SiameseNetwork2(nn.Module):
         refSpecInfo = refSpecInfo.view(refSpecInfo.size(0), -1)  # 改变数据的形状，-1表示不确定，视情况而定
 
         output = torch.cat((preInfo, fragInfo, refSpecInfo), 1)
-        # output = self.dropout(output)
         output = self.fc2(output)
-        output = F.selu(output)
         return output
 
     def forward(self, spectrum01, spectrum02):
@@ -115,15 +101,12 @@ class RawDataSet01():
         self.mgf_dataset = []
         self.load_file(mgf_file, csv_file)
         self.transform(saveName)
-        # pd.DataFrame(self.mgf_dataset).to_csv("../Data/train.data", header=None, index=None)
 
     def load_file(self, mgf_path, csv_path):
         print('Start to load file data...')
         info = pd.read_csv(csv_path, header=None)
         self.pairs_num = info.shape[0]
         self.spectrum1 = info[0].tolist()
-        # self.spectrum2 = info[1].tolist()
-        # self.label = info[2].tolist()
         for mgf in read(mgf_path, convert_arrays=1):
             self.MGF[mgf.get('params').get('title').replace('id=', '')] = mgf
         print('Finish to load data...')
@@ -317,194 +300,13 @@ class Dataset_RawDataset(data.dataset.Dataset):
     def __len__(self):
         return len(self.mgf_dataset)
 
-class NewMGFDataSet(data.dataset.Dataset):
-
-    def __init__(self, mgf_file, csv_file, reference_spectra_number=500):
-        if not os.path.exists(mgf_file):
-            raise RuntimeError("Can not find mgf file: '%s'" % mgf_file)
-        if not os.path.exists(csv_file):
-            raise RuntimeError("Can not find csv file: '%s'" % csv_file)
-        self.MGF = {}
-        self.mgf_dataset = []
-        self.load_file(mgf_file, csv_file)
-        self.transform(reference_spectra_number)
-        # print(self.mgf_dataset)info
-
-    def load_file(self, mgf_path, csv_path):
-        print('Start to load file data...')
-        info = pd.read_csv(csv_path, sep=",", header=None, index_col=None)
-        self.spectrum1 = info[0].tolist()
-        for mgf in read(mgf_path, convert_arrays=1):
-            #只是选出scan作为id
-            title = mgf.get('params').get('title').replace('id=', '')
-            # if len(title.split(",")) > 1:
-            #     NativeID = title.strip(" ").split(",")[1]
-            #     id = NativeID.strip(" ").split(":")[1]
-            #     final_id = id.strip("\"")
-            #     self.MGF[final_id] = mgf
-            # else:
-            self.MGF[title] = mgf
-
-        print('Finish to load data...')
-
-    def transform(self, reference_spectra_number):
-        print('Start to calculate data set...')
-
-        global spectrum_dict
-        spectrum_dict = {}
-        #五百个参考的谱图
-        # rfDataFrame = pd.read_csv("../Data/500RfSpectraBinData.csv", header=None, index_col=None)
-        # reference_intensity = rfDataFrame.values
-        rfData = read("../SpectraPairsData/gleams_reference_spectra.mgf", convert_arrays=1)
-        reference_intensity = np.array([self.bin_spectrum(r.get('m/z array'), r.get('intensity array')) for r in rfData])
-
-        peakslist1, precursor_feature_list1 = [], []
-        for s1 in self.spectrum1:
-            s1 = self.MGF[s1]
-            bin_s1 = self.bin_spectrum(s1.get('m/z array'), s1.get('intensity array'))
-            peakslist1.append(bin_s1)
-
-            mass1 = float(s1.get('params').get('pepmass')[0])
-            charge1 = int(s1.get('params').get('charge').__str__()[0])
-            mz1 = mass1 / charge1
-            precursor_feature1 = np.concatenate(
-                (self.gray_code(mass1), self.gray_code(mz1), self.charge_to_one_hot(charge1)))
-            precursor_feature_list1.append(precursor_feature1)
-
-        intensList01 = np.array(peakslist1)
-        refMatrix = np.transpose(reference_intensity)
-        DPList01 = np.dot(intensList01, refMatrix)
-
-        precursor_feature_list1 = np.array(precursor_feature_list1)
-
-        tmp01 = np.concatenate((DPList01, intensList01), axis=1)
-        spectrum01 = np.concatenate((tmp01, precursor_feature_list1), axis=1)
-
-        self.mgf_dataset = spectrum01
-
-        del self.MGF
-        print('Finish to calculate data set...')
-
-    def gray_code(self, number):
-        """
-        to get the gray code:\n
-            1. a = get the num's binary form
-            2. b = shift a one bit from left to right, put zero at the left position
-            3. gray code = a xor b
-            bin(num ^ (num >> 1))
-        :param number:
-        :return:np.array  gray code array for num
-        """
-        # assert num.is_integer(), 'Parameter "num" must be integer'
-        number = np.int(number)
-        # we need 27-bit "Gray Code"
-        bit = 27
-        shift = 1
-        gray_code = np.binary_repr(np.bitwise_xor(number, np.right_shift(number, shift)), bit)
-        # print(type(gray_code))
-        return np.asarray(' '.join(gray_code).split(), dtype=float)
-
-    def charge_to_one_hot(self, c: int):
-        """
-        encode charge with one-hot format for 1-7
-        :param c:
-        :return:
-        """
-        maximum_charge = 7
-        charge = np.zeros(maximum_charge, dtype=float)
-        # if charge bigger than 7, use 7 instead
-        if c > maximum_charge: c = maximum_charge
-        charge[c - 1] = c
-        return charge
-
-    def get_bin_index(self, mz, min_mz, bin_size):
-        relative_mz = mz - min_mz
-        return max(0, int(np.floor(relative_mz / bin_size)))
-
-    def bin_spectrum(self, mz_array, intensity_array, max_mz=2500, min_mz=50.5, bin_size=1.0005079):
-        """
-        bin spectrum and this algorithm reference from 'https://github.com/dhmay/param-medic/blob/master/parammedic/binning.pyx'
-        :param mz_array:
-        :param intensity_array:
-        :param max_mz:
-        :param min_mz:
-        :param bin_size:
-        :return:
-        """
-        key = mz_array.__str__()
-        if key in spectrum_dict.keys():  # use cache just take 4s
-            # if False: use the old one may take 7s for 50
-            return spectrum_dict[key]
-        else:
-            nbins = int(float(max_mz - min_mz) / float(bin_size)) + 1
-            results = np.zeros(nbins)
-            for index in range(len(mz_array)):
-                mz = mz_array[index]
-                intensity = intensity_array[index]
-                #进行平方根变换
-                intensity = np.math.sqrt(intensity)
-                if mz < min_mz or mz > max_mz:
-                    continue
-                bin_index = self.get_bin_index(mz, min_mz, bin_size)
-
-                if bin_index < 0 or bin_index > nbins - 1:
-                    continue
-
-                if results[bin_index] == 0:
-                    results[bin_index] = intensity
-                else:
-                    results[bin_index] += intensity
-                # results[bin_index] = intensity
-
-            intensity_sum = results.sum()
-            if intensity_sum > 0:
-                results /= intensity_sum
-                spectrum_dict[key] = results
-            else:
-                logging.debug('zero intensity found')
-        return results
-
-    def __getitem__(self, item):
-        return self.mgf_dataset[item]
-
-    def __len__(self):
-        return len(self.mgf_dataset)
-
 class EmbedDataSet():
     def __init__(self):
         self.out_list = []
 
-    def createSpecPairsFromMgfFile(self, mgfFile, storeCsvFile):
-        print("Start Embedding ...")
-        SpecPair = {}
-
-        data = read(mgfFile, convert_arrays=1)
-
-        for dt in data:
-            title = dt.get('params').get('title').replace('id=', '')
-            # print(title)
-            # t_01 = str(title).replace(" N", "N").replace(" ", "_")
-            # if len(title.split(",")) > 1:
-            #     NativeID = title.strip(" ").split(",")[1]
-            #     id = NativeID.strip(" ").split(":")[1]
-            #     final_id = id.strip("\"")
-            #     SpecPair["spectrum"] = final_id
-            # else:
-            #     SpecPair["spectrum"] = title
-            SpecPair["spectrum"] = title
-
-            SpecPair["charge"] = int(dt.get('params').get('charge').__str__()[0])
-            SpecPair["pepmass"] = float(dt.get('params').get('pepmass')[0])
-
-            df = pd.DataFrame(SpecPair, columns=["spectrum", "charge", "pepmass"], index=[0])
-            df.to_csv(storeCsvFile, mode="a+", header=False, index=False)
-            # del df
-            SpecPair.clear()
-
     def embedding_dataset(self, model, raw_data, mgfFile, storeCsvFile, storeEmbedFile):
 
         batch = 1
-        # net = torch.load("../SpectraPairsData/0423_150_128_01_model.pkl", map_location='cpu')
         net = torch.load(model, map_location='cpu')
 
         #生成csv文件，在存在的情况下不需要执行
@@ -522,15 +324,9 @@ class EmbedDataSet():
 
             spectrum01 = test_data.reshape(test_data.shape[0], 1, test_data.shape[1])
 
-            # input1_1 = spectrum01[:, :, :50]
-            # input1_2 = spectrum01[:, :, 50:2499]
-            # input1_3 = spectrum01[:, :, 2499:]
-            input1_1 = spectrum01[:, :, :100]
-            input1_2 = spectrum01[:, :, 100:2549]
-            input1_3 = spectrum01[:, :, 2549:]
-            # input1_1 = spectrum01[:, :, :500]
-            # input1_2 = spectrum01[:, :, 500:2949]
-            # input1_3 = spectrum01[:, :, 2949:]
+            input1_1 = spectrum01[:, :, :500]
+            input1_2 = spectrum01[:, :, 500:2949]
+            input1_3 = spectrum01[:, :, 2949:]
 
             output01 = net.forward_once(input1_3, input1_2, input1_1)
 
@@ -548,14 +344,11 @@ class EmbedDataSet():
         self.out_list.clear()
 
 def All_Logistic_AUC(X, y):
-    # data = pd.read_csv("./data/test_auc_data_02.txt", header=None, index_col=None)
     X, y = np.array(X), np.array(y)
     X = X.reshape(-1, 1)
     y = y.reshape(-1, 1)
 
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=0)
-
-    # y_one_hot = label_binarize(y_test, np.arange(2))  # 装换成类似二进制的编码
 
     alpha = np.logspace(-2, 2, 20)  # 设置超参数范围
     model = LogisticRegressionCV(Cs=alpha, cv=3, penalty='l2')  # 使用L2正则化
@@ -604,13 +397,11 @@ def show_test01_roc(fpr_list, tpr_list, auc_list, saveName):
     # plt.xlabel('Iteration')
     # plt.ylabel('Loss')
     plt.savefig('./data/auc/' + saveName + '.jpg')
-    # plt.show()
     plt.show()
 
 def draw_auc_plot(auc_data, plot_name):
     data = pd.read_csv(auc_data, sep="\t", header=0, index_col=None)
     y = data["label"]
-    X_pearson = data["pearson"]
     # tmp_df = df[["Spectrum_Title", "best_matched_spectrum_title", "pearson", "spearman", "normalized_dot_score", "dot_score", "mean_squared_error"]]
     head_list = ["pearson", "spearman", "normalized_dot_score", "dot_score", "mean_squared_error", "m_eu_dis","g_eu_dis"]
     fpr_list, tpr_list, roc_auc_list = [], [], []
