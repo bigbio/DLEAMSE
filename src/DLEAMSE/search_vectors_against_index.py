270b500e6c361f01da73eb1b5aed9678da61e6c1
qinchunyuan <523824050@qq.com>
2020-03-04 14:23:22 +0800

    update range search function

--- src/DLEAMSE/search_vectors_against_index.py
+++ src/DLEAMSE/search_vectors_against_index.py
@@ -23,10 +23,11 @@ def commanline_args():
     # declare args
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--index_file', type=argparse.FileType('r'), required=True, help='input index file')
+    parser.add_argument('--index_usi_file', type=argparse.FileType('r'), required=True, help='input index usi file without header')
     parser.add_argument('-i', '--input_embedded_spectra', type=argparse.FileType('r'), required=True, help='input embedded spectra file(s)')
-    parser.add_argument('--k', type=int, help='k for kNN', default=5)
-    parser.add_argument('-t','--threshold', type=float, help='Threshold for similarity searching', default=0.32)
-    parser.add_argument('-o', '--output', type=argparse.FileType('w'), required=True,  help='output file (should have extension .h5)')
+    # parser.add_argument('--k', type=int, help='k for kNN', default=5)
+    parser.add_argument('-t', '--threshold', type=float, help='Threshold for similarity searching.', default=0.1)
+    parser.add_argument('-o', '--output', type=argparse.FileType('w'), required=True,  help='output file, .csv)')
     return parser.parse_args()
 
 class Faiss_write_index():
@@ -147,18 +148,21 @@ class Faiss_Index_Search():
         D = D ** 0.5
         return D, I
 
-    def range_search(self, index_path, embedded, usi_data, threshold=0.32):
+    def range_search(self, index_path, embedded, usi_data, threshold=0.1, outpath="faiss_range_search_result.csv"):
         """
         Range Search can only use in CPU
+        :param output_path:
         :param index_path:
         :param embedded:
         :param usi_data:
         :return:
         """
-
+        print("loading index file...")
         index = faiss.read_index(index_path)  #cpu
+        print("loading index usi file...")
         index_usi = pd.read_csv(usi_data, header=None).values.tolist()
-        dist = threshold ** 2   #Threshold
+        dist = threshold   #Threshold
+        # dist = 0.32 ** 2   #Threshold
         query_id, limit_num, result_list = [], [], []
         print(embedded.shape[0])
         for i in range(embedded.shape[0]):
@@ -171,9 +175,7 @@ class Faiss_Index_Search():
             result_list.append(result_dict)
         result_df = pd.DataFrame({"query_id": query_id, "limit_num": limit_num, "result": result_list},
                                  columns=["query_id", "limit_num", "result"])
-        result_df.to_csv("./Human_RangeSearch.txt", index=False)
-        # pd.DataFrame(result_df).to_excel("./Human_RangeSearch.xls", index=False)
-    # print(result_df)
+        result_df.to_csv(outpath, index=False)
 
     def write_search_results(self, D, I, outpath):
         with h5py.File(outpath, 'w') as h5f:
@@ -181,9 +183,9 @@ class Faiss_Index_Search():
             h5f.create_dataset('D', data=D, chunks=True)
             h5f.create_dataset('I', data=I, chunks=True)
 
-    def execute_faiss_index_search(self, args):
+    def execute_knn_search(self, args):
 
-        print("loading index file...")
+
         index = self.read_faiss_index(args.index_file.name)
 
         print("loading embedded spectra vector...")
@@ -192,51 +194,28 @@ class Faiss_Index_Search():
         embedded_arrays.append(run_spectra)
         embedded_spectra = np.vstack(embedded_arrays)
         print("  Read a total of {} spectra".format(embedded_spectra.shape[0]))
-        # D, I = self.search_index(index, embedded_spectra.astype('float32'), args.k)
-        self.search_index_with_threshold(index, embedded_spectra.astype('float32'))
+        D, I = self.knn_search(index, embedded_spectra.astype('float32'), args.k)
         print("Writing results to {}...".format(args.output.name))
         # self.write_search_results(D, I, args.output.name)
         print("Wrote output file.")
         args.output.close()
 
-def test_faiss_range_search(index_path, embedded, usi_data):
-    # d = 32
-    # index = faiss.IndexFlatL2(d)
-    # index.add(data)
-    # index = read_faiss_index(index_path)
-    index = faiss.read_index(index_path)
-    index_usi = pd.read_csv(usi_data, header=None).values.tolist()
-    # dist = float(np.linalg.norm(data[3] - data[0])) * 0.99  # 定义一个半径/阈值
-    dist = 0.32**2  # 定义一个半径/阈值
-    query_id, limit_num, result_list = [], [], []
-    print(embedded.shape[0])
-    for i in range(embedded.shape[0]):
-        print(i)
-        res_index = index.range_search(embedded[[i], :], dist)  # 用第50个向量查询
-        query_id.append(index_usi[i][0])
-        limit_num.append(res_index[0][1])
-        result_dict = {}
-        for j in range(len(res_index[1])):
-            result_dict[index_usi[int(res_index[2][j])][0]] = res_index[1][j]
-        result_list.append(result_dict)
-    result_df = pd.DataFrame({"query_id": query_id, "limit_num":limit_num, "result":result_list}, columns=["query_id", "limit_num", "result"])
-    result_df.to_csv("./Human_RangeSearch.txt", index=False)
-    # pd.DataFrame(result_df).to_excel("./Human_RangeSearch.xls", index=False)
-    # print(result_df)
+    def execute_range_search(self, args):
+
+        print("loading embedded spectra vector...")
+        embedded_arrays = []
+        run_spectra = self.load_embedded_spectra_vector(args.input_embedded_spectra.name)
+        embedded_arrays.append(run_spectra)
+        embedded_spectra = np.vstack(embedded_arrays)
+        print("  Read a total of {} spectra".format(embedded_spectra.shape[0]))
+
+        self.range_search(args.index_file.name, embedded_spectra.astype('float32'), args.index_usi_file.name, args.threshold, args.output.name)
+        print("Writing results to {}...".format(args.output.name))
+        print("Wrote output file.")
+        args.output.close()
 
 if __name__ == "__main__":
-    # args = commanline_args()
+    args = commanline_args()
 
-    # index_searcher = Faiss_Index_Search()
-    # index_searcher.execute_faiss_index_search(args)
-
-    # index = "./PXD003552_61576_ArchiveSpectrum.index"
-    # data = np.load("./PXD003552_61576_ArchiveSpectrum_embedded.npy")
-    # data = data.astype('float32')
-    # usi_data = './PXD003552_61576_ArchiveSpectrum_spectra_usi.txt'
-
-    index = "./Human.index"
-    data = np.load("./Human_embedded.npy")
-    data = data.astype('float32')
-    usi_data = './Human_spectra_title.txt'
-    test_faiss_range_search(index, data, usi_data)
+    index_searcher = Faiss_Index_Search()
+    index_searcher.execute_range_search(args)
