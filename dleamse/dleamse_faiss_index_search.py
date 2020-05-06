e03919a50cc8a9fb394d08c8913d0a5f934fd39f
qinchunyuan <523824050@qq.com>
2020-05-06 20:33:15 +0800

    Minor changes of range_search output data's format

--- dleamse/dleamse_faiss_index_search.py
+++ dleamse/dleamse_faiss_index_search.py
@@ -5,6 +5,7 @@ Search a file full of embedded spectra against a faiss index, and save the resul
 
 import argparse
 import ast
+import json
 import os
 import sys
 
@@ -131,7 +132,126 @@ class FaissIndexSearch:
                              columns=["query_id", "limit_num", "result"])
     result_df.to_csv(outpath, index=False)
 
-  def execute_range_search(self, index_file, index_ids_usi_file, embedded_spectra_file, threshold, output_file):
+  def upper_range_search(self, index_path, index_ids_usi_file, embedded, lower_t, threshold, outpath="faiss_range_search_result.csv"):
+    """
+        Range Search can only use in CPU
+        :param outpath:
+        :param threshold: similarity thershold
+        :param index_path: index path
+        :param embedded: embeded file
+        :return:
+        """
+
+    print("loading index file...")
+    index = faiss.read_index(index_path)  # cpu
+    dist = threshold  # Threshold
+    query_id, limit_num, result_list = [], [], []
+    print("The number of query embedded spectra : {}".format(embedded.shape[0]))
+    result = index.range_search(embedded, dist)
+    lower_result = index.range_search(embedded, lower_t)
+    limit, D, I = result[0], result[1], result[2]
+    lower_limit, lower_D, lower_I = lower_result[0], lower_result[1], lower_result[2]
+
+    index_ids_usi_df = pd.read_csv(index_ids_usi_file, index_col=None)
+    f = open(outpath, "w")
+
+    result_list = []
+    for i in range(embedded.shape[0]):
+
+      tmp_result_dict = {}
+      tmp_result_dict["query_index"] = i
+      query_id.append(i)
+      # limit_num.append(limit[i + 1] - limit[i])
+      upper_limit_num = limit[i + 1] - limit[i]
+      lower_limit_num = lower_limit[i + 1] - lower_limit[i]
+      if upper_limit_num == lower_limit_num:
+        tmp_I = I[limit[i]:limit[i + 1]]
+        tmp_D = D[limit[i]:limit[i + 1]]
+      else:
+        tmp_I, tmp_D = [], []
+        test_tmp_I = I[limit[i]:limit[i + 1]]
+        test_tmp_D =  D[limit[i]:limit[i + 1]]
+        test_l_I = lower_I[lower_limit[i]:lower_limit[i + 1]]
+        test_l_D = lower_D[lower_limit[i]:lower_limit[i + 1]]
+        for k in range(upper_limit_num):
+          if test_tmp_I[k] in test_l_I:
+            continue
+          else:
+            tmp_I.append(test_tmp_I[k])
+            tmp_D.append(test_tmp_D[k])
+
+      tmp_result_list = []
+      for j in range(upper_limit_num - lower_limit_num):
+        tmp_result = {}
+        key = index_ids_usi_df.loc[index_ids_usi_df["ids"] == tmp_I[j]]["usi"].values[0]
+        tmp_result["usi"] = str(key)
+        tmp_result["similarity_score"] = str(tmp_D[j])
+        tmp_result_list.append(tmp_result)
+
+      tmp_result_dict["result"] = tmp_result_list
+
+      result_list.append(tmp_result_dict)
+
+    json_data = json.dumps(result_list)
+    # print(json_data)
+    f.write(json_data)
+    f.close()
+
+      # jsObject = json.dumps(result_list, cls=NumpyEncoder)
+      # f = open(outpath, "w")
+      # f.write(jsObject)
+
+    # result_df = pd.DataFrame({"query_id": query_id, "limit_num": limit_num, "result": result_list},
+    #                          columns=["query_id", "limit_num", "result"])
+    # result_df.to_csv(outpath, index=False)
+
+  def new_range_search(self, index_path, index_ids_usi_file, embedded, threshold, outpath="faiss_range_search_result.csv"):
+    """
+        Range Search can only use in CPU
+        :param outpath:
+        :param threshold: similarity thershold
+        :param index_path: index path
+        :param embedded: embeded file
+        :return:
+        """
+
+    print("loading index file...")
+    index = faiss.read_index(index_path)  # cpu
+    dist = threshold  # Threshold
+
+    print("The number of query embedded spectra : {}".format(embedded.shape[0]))
+    result = index.range_search(embedded, dist)
+
+    limit, D, I = result[0], result[1], result[2]
+
+    index_ids_usi_df = pd.read_csv(index_ids_usi_file, index_col=None)
+    f = open(outpath, "w")
+
+    result_list = []
+    for i in range(embedded.shape[0]):
+
+      tmp_result_dict = {}
+      tmp_result_dict["query_index"] = i
+      tmp_I = I[limit[i]:limit[i + 1]]
+      tmp_D = D[limit[i]:limit[i + 1]]
+
+      tmp_result_list = []
+      for j in range(limit[i + 1] - limit[i]):
+        tmp_result = {}
+        key = index_ids_usi_df.loc[index_ids_usi_df["ids"] == tmp_I[j]]["usi"].values[0]
+        tmp_result["usi"] = str(key)
+        tmp_result["similarity_score"] = str(tmp_D[j])
+        tmp_result_list.append(tmp_result)
+
+      tmp_result_dict["result"] = tmp_result_list
+
+      result_list.append(tmp_result_dict)
+
+    json_data = json.dumps(result_list)
+    f.write(json_data)
+    f.close()
+
+  def execute_range_search(self, index_file, index_ids_usi_file, embedded_spectra_file, lower_threshold, upper_threshold, output_file):
 
     print("loading embedded spectra vector...")
     # embedded_arrays = []
@@ -139,9 +259,16 @@ class FaissIndexSearch:
     # embedded_arrays.append(run_spectra)
     # embedded_spectra = np.vstack(embedded_arrays)
     print("  Read a total of {} spectra".format(run_spectra.shape[0]))
-
-    self.range_search(index_file, index_ids_usi_file, run_spectra.astype('float32'), threshold, output_file)
-    print("Wrote results to {}...".format(output_file))
+    if lower_threshold == 0 or lower_threshold == upper_threshold:
+      print("Runing range search ...")
+      self.new_range_search(index_file, index_ids_usi_file, run_spectra.astype('float32'), threshold, output_file)
+      print("Wrote results to {}...".format(output_file))
+    elif 0 < lower_threshold < upper_threshold:
+      print("Runing upper range search ...")
+      self.upper_range_search(index_file, index_ids_usi_file, run_spectra.astype('float32'), lower_threshold, threshold, output_file)
+      print("Wrote results to {}...".format(output_file))
+    else:
+      print("Wrong lower_threshold value, please enter a correct threshold value.")
 
 
 if __name__ == "__main__":
@@ -153,3 +280,7 @@ if __name__ == "__main__":
 
   index_searcher = FaissIndexSearch()
   index_searcher.execute_range_search(index_file, index_ids_usi_file, embedded_spectra, threshold, output)
+
+  # data = [{'a':1, 'b':2, 'c':[{'a1':1, 'b1':2, 'c1':3}, {'a2':1, 'b2':2, 'c2':3}]}, {'a1':1, 'b1':2, 'c1':3}]
+  # json_data = json.dumps(data)
+  # print(json_data)
